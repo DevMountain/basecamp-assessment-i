111679ea2171e457a379facbff1422accaa4f7cb
starslasher1515 <39746922+starslasher1515@users.noreply.github.com>
2018-11-04 23:09:41 -0500

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -11,36 +11,89 @@ var faveNum = 15;
 var lovesCode = true;
 
 // #4 Using the previously created variables, create a new variable called 'me' that uses the myName and faveNum varible to create a sentence like this 'My name is Bryan and my favorite number is 3'
+var me = "My name is " + myName + " and my favorite number is " +faveNum;
+console.log(me);
 
-var me = "My Name is "+ myName +" and my favorite number is" +faveNum+ "."; 
 
-// #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
+/* create a new variable called 'trueFaveNum'.
+should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator */
 
-var trueFaveNum = 
+var trueFaveNum = true;
+function getFaveNum(){
+  if(trueFaveNum === faveNum){
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}{
+  getFaveNum();
+}
 
-// #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
+
+
+/* #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string
+'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'*/
+
+var notHakeem = true;
+if(myName !== notHakeem){
+  console.log(myName);
+} else if(myName === "Hakeem Olajuwon"){
+  console.log(myName);
+} else {()
+  console.log(myName);
+}
+
+
 
 // CODE HERE
 
-// #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
+/* #7 Create an if statement that checks the age variable below.
+If age is greater than 16, then set canDrive to true otherwise set canDrive to false.
+The canDrive variable will be set by the unit test so do not adjust it. */
+
 var age = 18;
-var canDrive;
+var canDrive = true;
+if(age >= 16){
+  console.log(canDrive);
+} else {
+  console.log(canDrive);
+}
 
 
 // CODE HERE
 
-// #8 Create an if statement inside of the responseCreator function that sets the colorResponse variable value based off of a passed in value. The param value passed in will be added in the unit test. If the param value is 'green' set colorResponse variable to 'Green is okay'. If the param value is 'red' set reponse variable to 'I heart red'. Otherwise set the colorResponse variable to 'What is your favorite color?'. Make sure to return the colorResponse variable at the end once it has been properly set.
+/* #8 Create an if statement inside of the responseCreator function that sets the colorResponse variable value based off of a passed in value.
+The param value passed in will be added in the unit test. If the param value is 'green' set colorResponse variable to 'Green is okay'.
+If the param value is 'red' set reponse variable to 'I heart red'. Otherwise set the colorResponse variable to 'What is your favorite color?'.
+Make sure to return the colorResponse variable at the end once it has been properly set. */
 
 var colorResponse;
 
 var responseCreator = function(param){
-  // CODE HERE
-}
+  if(param  == 'green'){
+  colorResponse ='Green is okay';
+
+  } else if (param == 'red') {
+    colorResponse = 'I heart red';
+
+  }else{
+  colorResponse ='What is your favorite color?';
+   }
+   return colorResponse
+  }
+
 
 // #9 Create a function called 'sum' that takes in one parameter and returns the sum of that parameter added it itself.
 
-// CODE HERE
+function sum(param){
+  return param + param;
+};
 
-// #10 Create a function called 'greeter' that takes in two parameters, name and number. Have 'greeter' return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values..
+/* #10 Create a function called 'greeter' that takes in two parameters, name and number.
+Have 'greeter' return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values..*/
 
-// CODE HERE
+function greeter(name,age){
+  return 'Hi! I am ' + name + ' and I am ' + age + ' years old';
+}
+
+greeter(name,age)
