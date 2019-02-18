8828eb8285bf88d1c3c12f319adce0259ecfd37a
Jeff Pinckney <jeffpinckney.90@gmail.com>
2019-02-18 12:37:29 -0700

    Reworded some questions and added more grammar consistency

--- assessment.js
+++ assessment.js
@@ -17,7 +17,7 @@
 /**
  * #3
  * 
- * Create a variable called 'lovesCode' and set it to true
+ * Create a variable called 'lovesCode' and assign it to the boolean value true
  */
 
 // CODE HERE
@@ -25,7 +25,8 @@
 /**
  * #4
  * 
- * Create a new variable called 'me' and assign it a sentence that uses the 'myName' and 'faveNum' variables. 
+ * Create a new variable called 'me'. 
+ * Assign it a string that uses the 'myName' and 'faveNum' variables. 
  * Example: My name is Bryan and my favorite number is 3.
  */
 
@@ -34,8 +35,8 @@
 /**
  * #5
  * 
- * Using the previously created faveNum variable, create a new variable called 'trueFaveNum'.
- * 'trueFaveNum' should equal true when comparing faveNum to your favorite number.
+ * Create a new variable called 'trueFaveNum'.
+ * 'trueFaveNum' should equal true when comparing the value of the 'faveNum' variable.
  * HINT: Use a comparison operator
  */
 
@@ -44,10 +45,9 @@
 /**
  * #6
  * 
- * Create a new variable called 'notHakeem'. Using the previously created 'myName' variable
- * to test that your name DOES NOT equal the string "Hakeem Olajuwon".
- * If your name does happen to be Hakeem Olajuwon, test against "Karl Malone"
- * HINT: Use a logical operator
+ * Create a new variable called 'notHakeem'. 
+ * Assign this variable the result of a comparison 
+ * between the 'myName' variable and the string "Hakeem Olajuwon"
  */
 
 // CODE HERE
@@ -56,8 +56,8 @@
  * #7
  * 
  * Create an if statement that checks the 'age' variable below.
- * If age is greater than 16, set canDrive to true otherwise set canDrive to false.
- * The 'canDrive' variable will be set by the unit test so do not adjust it.
+ * If age is greater than 16, assign 'canDrive' to true otherwise assign 'canDrive' to false.
+ * The 'canDrive' variable will be assigned by the unit test so do not adjust it.
  */
 
 var age = 18;
@@ -69,12 +69,12 @@ var canDrive;
  * #8
  * 
  * Create an if statement inside of the 'responseCreator' function. 
- * Set the 'colorResponse' variable value based off of a passed in value. 
- * The param value passed in will be added in the unit test.
- * If the param value is 'green' set 'colorResponse' variable to 'Green is okay'.
- * If the param value is 'red' set response variable to 'I heart red'.
- * Otherwise set the 'colorResponse' variable to 'What is your favorite color?'.
- * Make sure to return the 'colorResponse' variable at the end once it has been properly set.
+ * Assign the 'colorResponse' variable the value based off of a passed in parameter. 
+ * The parameter value passed in will be added in the unit test.
+ * If the parameter value is "green" assign the 'colorResponse' variable to "Green is okay".
+ * If the parameter value is "red" assign the 'colorResponse' variable to "I heart red".
+ * Otherwise assign the 'colorResponse' variable to the string "What is your favorite color?".
+ * Return the 'colorResponse' variable at the end of the function.
  */
 
 var colorResponse;
@@ -98,7 +98,7 @@ var responseCreator = function (parameter) {
  * 
  * Create a function called 'greeter' that takes in two parameters, name and number.
  * Have 'greeter' return a string that says something like this:
- * 'Hi! I am Bryan and I am 28 years old" 
+ * "Hi! I am Bryan and I am 28 years old" 
  */  
 
 // CODE HERE
